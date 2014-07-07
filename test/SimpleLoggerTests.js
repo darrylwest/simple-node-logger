ea30eccb44ee15b39269d2e5382d3edd3a1aa2d8
darryl west <darryl.west@RainCitySoftware.com>
2014-07-07 10:07:18 -0700

    refactored for simple logger

---
+++ test/SimpleLoggerTests.js
@@ -0,0 +1,40 @@
+/**
+ *
+ *
+ * @author: darryl.west@roundpeg.com
+ * @created: 7/7/14 9:44 AM
+ */
+var should = require('chai').should(),
+    dash = require( 'lodash' ),
+    casual = require( 'casual' ),
+    Logger = require('../lib/Logger' ),
+    SimpleLogger = require( '../lib/SimpleLogger');
+
+describe('SimpleLogger', function() {
+    'use strict';
+
+    var createOptions = function() {
+        var opts = {};
+
+        return opts;
+    };
+
+    describe('#instance', function() {
+        var logger = new SimpleLogger( createOptions() ),
+            methods = [
+                'createLogger'
+            ];
+
+        it('should create an instance of SimpleLogger', function() {
+            should.exist( logger );
+            logger.should.be.instanceof( SimpleLogger );
+        });
+
+        it('should have all expected methods by size and type', function() {
+            dash.methods( logger ).length.should.equal( methods.length );
+            methods.forEach(function(method) {
+                logger[ method ].should.be.a( 'function' );
+            });
+        });
+    });
+});
