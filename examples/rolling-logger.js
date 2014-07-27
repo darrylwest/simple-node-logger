1971b272b8863c04256aa58ff36bc308d24ba672
darryl west <darryl.west@RainCitySoftware.com>
2014-07-27 14:10:23 -0700

    implemented example

---
+++ examples/rolling-logger.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+
+var opts = {
+    logDirectory: __dirname + '/../logs',
+    fileNamePattern: 'apptest-<date>.log',
+    dateFormat:'YYYY.MM.DD-HHa'
+};
+
+var log = require('../lib/SimpleLogger').createRollingFileLogger( opts );
+
+// write some stuff...
+log.trace('this is a simple trace log statement (should not show)');
+log.debug('this is a simple debug log statement (should not show)');
+log.info('this is a simple info log statement/entry');
+log.warn('this is a simple warn log statement/entry');
+log.error('this is a simple error log statement/entry');
+log.fatal('this is a simple fatal log statement/entry');
+
+log.info('set the level to all');
+log.setLevel('all');
+log.trace('this is a simple trace log statement (should show)');
+log.debug('this is a simple debug log statement (should show)');
+
+var appender = log.getAppenders()[0];
+console.log('write to file: ', appender.__protected().currentFile );
--- examples/simple-file-logger.js
+++ examples/simple-file-logger.js
@@ -5,11 +5,11 @@ console.log('opening log file: ', filename);
 
 var log = require('../lib/SimpleLogger').createSimpleFileLogger( filename );
 
-log.trace('this is a simple trace log statement (should not show)');
-log.debug('this is a simple debug log statement (should not show)');
 log.info('this is a simple info log statement/entry');
 log.warn('this is a simple warn log statement/entry');
 log.error('this is a simple error log statement/entry');
+log.trace('this is a simple trace log statement (should not show)');
+log.debug('this is a simple debug log statement (should not show)');
 log.fatal('this is a simple fatal log statement/entry');
 
 log.info('set the level to all');
