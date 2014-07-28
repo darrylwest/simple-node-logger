2f4dfde605ad5edc5e634de9ecff57ffafadac02
darryl west <darryl.west@RainCitySoftware.com>
2014-07-27 20:50:38 -0700

    exposed the appender classes

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ module.exports = require('./lib/SimpleLogger');
 module.exports.AbstractAppender = require('./lib/AbstractAppender');
 module.exports.Logger = require('./lib/Logger');
 
+module.exports.appenders = {
+    ConsoleAppender:require('./lib/ConsoleAppender'),
+    FileAppender:require('./lib/FileAppender'),
+    RollingFileAppender:require('./lib/RollingFileAppender')
+};
+
 module.exports.mocks = {
     MockAppender:require('./test/mocks/MockAppender'),
     MockLogger:require('./test/mocks/MockLogger')
 };
+
