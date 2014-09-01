b99d63e9cac894cef70f34c924b0d26fd004d5fa
darryl west <darryl.west@RainCitySoftware.com>
2014-09-01 13:54:02 -0700

    tested new object serialization against examples

--- examples/domain-logger.js
+++ examples/domain-logger.js
@@ -18,11 +18,11 @@ manager.addAppender( manager.createFileAppender( { logFilePath:filename } ));
 log1 = manager.createLogger( 'CategoryOne' );
 log2 = manager.createLogger( 'CategoryTwo' );
 
-log1.trace('this is a simple trace log statement (should not show)');
+log1.trace('this is a simple info log statement/entry: ', opts);
 log1.debug('this is a simple debug log statement (should not show)');
-log1.info('this is a simple info log statement/entry');
-log2.warn('this is a simple warn log statement/entry');
-log1.error('this is a simple error log statement/entry');
+log1.info('this is a simple trace log statement: ', { thedate:new Date() }, ', now: ', new Date(), 1,2,3 );
+log2.trace('this is a simple warn log statement/entry');
+log1.error('this is a simple error log statement/entry: ', manager, 4,5);
 log2.fatal('this is a simple fatal log statement/entry');
 
 log2.trace('this is a simple trace log statement (should show)');
