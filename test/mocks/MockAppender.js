b36b6eb1650d12d67978db36dc2be97938f0622f
slattery <mike@actualfood.com>
2015-12-14 16:58:00 -0500

    (tests): Add levels and setLevel to MockAppender

--- test/mocks/MockAppender.js
+++ test/mocks/MockAppender.js
@@ -6,10 +6,20 @@
  */
 var MockAppender = function() {
     'use strict';
-
+    var Logger = require('../lib/Logger' );
+    var level  = Logger.DEFAULT_LEVEL,
+        levels = Logger.STANDARD_LEVELS,
+        currentLevel = levels.indexOf( level );
     var appender = this;
 
     this.entries = [];
+
+    this.setLevel = function(level) {
+        var idx = levels.indexOf( level );
+        if (idx >= 0) {
+            currentLevel = idx;
+        }
+    };
 
     this.write = function(entry) {
         appender.entries.push( entry );
