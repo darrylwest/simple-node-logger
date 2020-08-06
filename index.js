2077e6d3590a715f457aa51ca1c37f564f6f9842
v-electrolux <vasiliyz@gmail.com>
2020-08-06 15:49:10 +0300

    remove tests from npm package distribution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 /** darryl.west@raincitysoftware.com **/
 
+const fs = require( 'fs' );
+
 module.exports = require('./lib/SimpleLogger');
 module.exports.AbstractAppender = require('./lib/AbstractAppender');
 module.exports.Logger = require('./lib/Logger');
@@ -11,8 +13,10 @@ module.exports.appenders = {
     RollingFileAppender:require('./lib/RollingFileAppender')
 };
 
-module.exports.mocks = {
-    MockAppender:require('./test/mocks/MockAppender'),
-    MockLogger:require('./test/mocks/MockLogger')
-};
+if (fs.existsSync('./test/')) {
+    module.exports.mocks = {
+        MockAppender: require('./test/mocks/MockAppender'),
+        MockLogger: require('./test/mocks/MockLogger')
+    };
+}
 
